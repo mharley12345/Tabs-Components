b3dd15b3c9df2e8415bc336c05a7741923044dac
mharley12345 <49734550+mharely12345@users.noreply.github.com>
2019-05-23 15:49:50 -0700

    attempted stretch

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -11,18 +11,25 @@ class Dropdown {
     // assign the reference to the ".dropdown-content" class found in the dropdown element            
     this.content = this.element.querySelector('.dropdown-content')                                   
                                                                                                      
-    // Add a click handler to the button reference and call the toggleContent method.
+    // Add a click handler to the button reference and call the toggleContent method.   
     this.button.addEventListener('click', () => this.toggleContent())                                     
-                                                                      
-  }                                                                  
-                                                                    
-  toggleContent() {                                                
-                                                                  
-    // Toggle the ".dropdown-hidden" class off and on            
-    this.content.classList.toggle('dropdown-hidden');                               
-  }                                                           
-}                                                               
-                                                              
-                                                              
-// Nothing to do here, just study what the code is doing and move on to the Dropdown class
+                                                                               
+  }                                                                           
+                                                                                                    
+  toggleContent() {                                                                                 
+                                                                                                    
+    // Toggle the ".dropdown-hidden" class off and on                                               
+    this.content.classList.toggle('dropdown-hidden'); 
+    // stretch                       
+    TweenMax.to(".dropdown-content", 10, {x:1135, y:1, scale:1, skewX:0, rotation:0});                                                 
+  }                                                                                                 
+}                                                                                                   
+                                                                                                    
+                                                                                                    
+// Nothing to do here, just study what the code is doing and move on to the Dropdown class          
 let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+                                                                                    
+                                                                                    
+                                                                                  
+                                                                               
+                                                                                  
--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,5 +1,5 @@
 
-class TabLink {          
+class TabLink {                
   constructor(element) {
     // Assign this.element to the passed in DOM element
      this.element = element                      
@@ -26,38 +26,41 @@ class TabLink {
                                          
     // Add a class named "tabs-link-selected" to this link
     this.element.classList.add('tabs-link-selected');
-    
+                                                     
     // Call the select method on the item associated with this link
-    this.tabItem.select();
-  }
-}
-
-class TabItem {
-  constructor(element) {
+    this.tabItem.select();              
+  }                                     
+}                        
+                         
+class TabItem {          
+  constructor(element) {                                                                                             
     // Assign this.element to the passed in element
     this.element = element;                          
-  }
-   
-  select() {
+  }                                                  
+                       
+  select() {                                                                                      
     // Select all ".tabs-item" elements from the DOM
     const items = document.querySelectorAll('.tabs-item');                         
       
     // Remove the class "tabs-item-selected" from each element                                                        
-    items.forEach( item => item.classList.remove('tabs-item-selected'))
+    items.forEach( item => item.classList.remove('tabs-item-selected'))                                              
            
     // Add a class named "tabs-item-selected" to this element                                                         
     this.element.classList.add('tabs-item-selected');
   }                                      
-}                    
+}                                                                                                                    
                           
 /* START HERE:                                                                                                       
 - Select all classes named ".tabs-link" and assign that value to the links variable                                  
                                                                                                                      
 - With your selection in place, now chain a .forEach() method onto the links variable to iterate over the DOM NodeList
-                                         
+                                                                                                                     
 - In your .forEach() method's callback function, return a new instance of TabLink and pass in each link as a parameter
-                                         
-*/                                       
-                                         
-links = document.querySelectorAll('.tabs-link');
-links.forEach(link => new TabLink(link));                                                                            
+                                                                                                                     
+*/                                                                                                                   
+                                                                                                                     
+links = document.querySelectorAll('.tabs-link');                                                                     
+links.forEach(link => new TabLink(link));     
+                                          
+                                  
+                                  
