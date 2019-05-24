baf7a6442c1b32ef39b03b9deba0e2c203fc0b80
mharley12345 <49734550+mharely12345@users.noreply.github.com>
2019-05-23 20:40:49 -0700

    got the menu to go back to start

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,3 +1,4 @@
+ 
 class Dropdown {
   constructor(element) {
                         
@@ -8,28 +9,63 @@ class Dropdown {
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
     this.button = this.element.querySelector(".dropdown-button");                                         
                                                                                                       
-    // assign the reference to the ".dropdown-content" class found in the dropdown element            
-    this.content = this.element.querySelector('.dropdown-content')                                   
-                                                                                                     
-    // Add a click handler to the button reference and call the toggleContent method.   
-    this.button.addEventListener('click', () => this.toggleContent())                                     
-                                                                               
-  }                                                                           
-                                                                                                    
-  toggleContent() {                                                                                 
-                                                                                                    
-    // Toggle the ".dropdown-hidden" class off and on                                               
-    this.content.classList.toggle('dropdown-hidden'); 
-    // stretch                       
-    TweenMax.to(".dropdown-content", 10, {x:1135, y:1, scale:1, skewX:0, rotation:0});                                                 
-  }                                                                                                 
-}                                                                                                   
-                                                                                                    
-                                                                                                    
-// Nothing to do here, just study what the code is doing and move on to the Dropdown class          
-let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
-                                                                                    
-                                                                                    
-                                                                                  
-                                                                               
-                                                                                  
+    // assign the reference to the ".dropdown-content" class found in the dropdown element                                                                                                                            
+    this.content = this.element.querySelector('.dropdown-content')                                                                                                                                                   
+                                                                                                                                                                                                                    
+    // Add a click handler to the button reference and call the toggleContent method.                                                                                                                               
+                                                                                                                                                                                                                  
+    this.button.addEventListener('click', () => this.toggleContentOn())                                                                                                                                         
+                                                                                           
+  }                                                               
+                                                                                                                                                                                                            
+                                                                                                                                                                                                                                                                  
+                                                                                                                                                                                                                     
+                                                                                                                                                                                                                   
+                                                                                                                                                                                                                   
+  toggleContentOn() {                                                                                                                                                                                                                                                  
+    var myTweenMax = document.querySelector('.dropdown-content');                                                                                                                         
+                                                                                                                                                                                                                                                               
+    // Toggle the ".dropdown-hidden" class off and on                                                                                                                                                                                                                
+    this.content.classList.toggle('dropdown-hidden');                                                                                                                                                                                                               
+    // stretch                                                                                                                                                                                                                                                      
+    TweenMax.to(myTweenMax, 10, {x:1135, y:1, scale:1, skewX:0, rotation:0,},this.button.addEventListener('click',() => this.restart()));                                                                                                                  
+                                                                                                                                                                                                                                                                  
+  }                                    
+                                       
+ restart() {                                                                                                                                                                                                                                                  
+    var myTweenMax2 = document.querySelector('.dropdown-content');                                                                                                                                                                                                                                                             
+   TweenMax.to(myTweenMax2, 10, {x:0,y:0,scale:1,skewX:0,rotation:0})                                                                                                                                          
+   console.log('i work')                                                                                                                                                                                                                                     
+                                                                                                                                                                                                                                                            
+                                                                                                                                                                                                                                                           
+                                                                                                                                                                                                                                                          
+  };                                                                                                                                                                                                         
+                                                                                                                                                                        
+}                                                                                                                                                       
+                                                                                                                                                                                                                                                            
+                                                                                                                                                        
+                                                                                                                                                        
+                                                                                                                                                       
+                                                                                                                                                       
+                                                                                                                                                      
+                                                                                                                                                      
+           
+                        
+                                                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                                      
+                                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                                  
+                                                                                                                                                                                                                                                
+                                                                                                                                                                                                                                            
+                                                                                                                                                                                                                              
+                                                                                                                                                                                                                          
+                                                                                                                                                                                                                      
+                                                                                                                                                                                                                              
+                                                                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                                      
+                                                                                                                                                                                                                                                     
+// Nothing to do here, just study what the code is doing and move on to the Dropdown class                                                     
+let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));                           
+                                                                                                                
+                                                                                                             
+                                                                                                            
--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,13 +1,13 @@
 
-class TabLink {                
+class TabLink {                       
   constructor(element) {
     // Assign this.element to the passed in DOM element
      this.element = element                      
                                                  
-    // Get the custom data attribute on the Link
-    this.data = this.element.dataset.tab;       
+    // Get the custom data attribute on the Link                
+    this.data = this.element.dataset.tab;                                
                                                     
-    // Using the custom data attribute get the associated Item element
+    // Using the custom data attribute get the associated Item element                
     this.itemElement = document.querySelector(`.tabs-item[data-tab='${element.dataset.tab}']`);
                                                                                              
     // Using the Item element, create a new instance of the TabItem class                   
@@ -18,38 +18,38 @@ class TabLink {
   };                                                                                    
                                                                                        
   select() {                                                                          
-    // Get all of the elements with the tabs-link class
+    // Get all of the elements with the tabs-link class                             
     let links = document.querySelectorAll('.tabs-link');
-                                               
+                                                     
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     Array.from(links).forEach( item => item.classList.remove('tabs-link-selected'));
-                                         
+                                            
     // Add a class named "tabs-link-selected" to this link
     this.element.classList.add('tabs-link-selected');
                                                      
     // Call the select method on the item associated with this link
-    this.tabItem.select();              
-  }                                     
-}                        
-                         
-class TabItem {          
+    this.tabItem.select();                  
+  }                                         
+}                                           
+                                            
+class TabItem {                            
   constructor(element) {                                                                                             
     // Assign this.element to the passed in element
     this.element = element;                          
   }                                                  
-                       
+                                         
   select() {                                                                                      
     // Select all ".tabs-item" elements from the DOM
     const items = document.querySelectorAll('.tabs-item');                         
       
     // Remove the class "tabs-item-selected" from each element                                                        
-    items.forEach( item => item.classList.remove('tabs-item-selected'))                                              
+    items.forEach( item => item.classList.remove('tabs-item-selected'))                                               
            
     // Add a class named "tabs-item-selected" to this element                                                         
     this.element.classList.add('tabs-item-selected');
   }                                      
 }                                                                                                                    
-                          
+                                                                                                                     
 /* START HERE:                                                                                                       
 - Select all classes named ".tabs-link" and assign that value to the links variable                                  
                                                                                                                      
